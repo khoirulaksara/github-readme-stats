8a39c71d2178d9bf32afcb7b41e87ae11fb319ab
Khoirul Aksara <me@serat.us>
2025-03-14 21:34:21 +0700

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -462,7 +462,7 @@ export const themes = {
     icon_color: "ffffff",
     bg_color: "35,4158d0,c850c0,ffcc70",
   },
-  colorfull: {
+  berwarna: {
     title_color: "35,4158d0,c850c0,ffcc70",
     icon_color: "35,4158d0,c850c0,ffcc70",
     text_color: "35,4158d0,c850c0,ffcc70",
